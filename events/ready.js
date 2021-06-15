c072095994e85092581239207ecb5667c91b0a3f
DESKTOP-0BSVB30\kde12 <kde12327@gmail.com>
2021-06-15 23:56:22 +0900

    UPDATE coin system coin price almost around 100 won

--- events/ready.js
+++ events/ready.js
@@ -3,8 +3,8 @@ const db = require("../models");
 
 
 const COININTERVAL = 60000;
-const COINMAX = 3.1;
-const COINMIN = -3;
+// const COINMAX = 3.1;
+// const COINMIN = -3;
 
 
 module.exports = (client, message) => {
@@ -23,9 +23,13 @@ module.exports = (client, message) => {
           price: '100',
         },
       })
+      var COINMAX = 3 + 3 * ((100 - coin.price)/80);
+      var COINMIN = -3 + 3 * ((100 - coin.price)/80);
+
       var next = parseFloat(coin.price + coin.price * (Math.random() * (COINMAX - COINMIN) + COINMIN) / 100).toFixed(2);
       coin.price = (next > 0) ? next: 0;
       console.log('SEG coin price['+ new Date() +']: '+coin.price);
+      console.log(COINMAX, COINMIN);
       await coin.save();
       var segcjannel = client.channels.cache.get('836539076785078272');
       segcjannel.send('SEG coin price['+ new Date() +']: '+coin.price);
